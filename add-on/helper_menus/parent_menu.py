075f0c99798507793b68f72d8a2829944c709bcf
aggregate1166877 <noreply@frostoven.com>
2024-08-29 03:37:46 +1200

    Add contextual editing icons.

--- add-on/helper_menus/parent_menu.py
+++ add-on/helper_menus/parent_menu.py
@@ -13,20 +13,20 @@ class CosmosisParentMenu(bpy.types.Menu):
     """
     Parent Menu for Cosmosis Mesh Types
     """
-    bl_idname = "VIEW3D_MT_cosmosis_parent_menu"
-    bl_label = "Cosmosis Mesh Types"
+    bl_idname = 'VIEW3D_MT_cosmosis_parent_menu'
+    bl_label = 'Cosmosis Mesh Types'
 
     def draw(self, context):
         layout = self.layout
 
         # Check if a specific mesh type is selected.
-        selected_type = context.object.get("csmType", None)
+        selected_type = context.object.get('csmType', None)
         if selected_type:
             # We store types as camelCase. Convert to a snake_case to follow Blender standards.
             snake_name = to_snake_case(selected_type)
 
             # Show the specific menu for the selected mesh type.
-            menu_object = layout.operator(f'object.csm_{snake_name}')
+            menu_object = layout.operator(f'object.csm_{snake_name}', icon='GREASEPENCIL')
 
             # This can happen if the plugin is out of date, or if a modder did something weird.
             if menu_object is None:
@@ -36,7 +36,7 @@ class CosmosisParentMenu(bpy.types.Menu):
                 )
 
             # Add a button to clear the type information
-            layout.operator("object.csm_clear_mesh_type", text="Clear Type Info")
+            layout.operator('object.csm_clear_mesh_type', text='Clear Type Info', icon='PANEL_CLOSE')
         else:
             # Show all enable mesh code type menu items.
             for menu_item in enabled_menu_items:
